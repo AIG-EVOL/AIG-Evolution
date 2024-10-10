0b05edf5dd06f15366e294a40bfb9c94e36c1d9b
AIG-Evolution <databuxx@live.fr>
2024-10-10 13:50:37 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,12 +255,7 @@
 					// Trigger resize (sidebar lock).
 						$window.triggerHandler('resize.sidebar-lock');
 
-					document.querySelectorAll('.opener').forEach(opener => {
-    opener.addEventListener('click', function() {
-        // Toggle le sous-menu suivant
-        const submenu = this.nextElementSibling;
-        if (submenu) {
-            submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+					style.display === 'block' ? 'none' : 'block';
         }
            });
 });
